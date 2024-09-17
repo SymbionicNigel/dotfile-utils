bc8c1eaaf6969ce471e36d1a337a5f58a46810a8
Nigel Krajcer <nphilmont2010@gmail.com>
2024-09-16 22:09:03 -0400

    feat: initialize script updated

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,42 +1,5 @@
 #!/bin/bash Bash
 
-install_gpg() {
-    # Test if GPG is installed, return if true
-    which gpg > /dev/null
-    if [ $? == 0 ]; then
-        echo "GPG already installed"
-        return 0
-    fi
-
-    # Package manager parsing pulled from
-    # https://unix.stackexchange.com/questions/46081/identifying-the-system-package-manager
-    declare -A osInfo;
-    osInfo[/etc/redhat-release]=yum
-    osInfo[/etc/arch-release]=pacman
-    osInfo[/etc/gentoo-release]=emerge
-    osInfo[/etc/SuSE-release]=zypp
-    osInfo[/etc/debian_version]=apt-get
-    osInfo[/etc/alpine-release]=apk
-    PKG_MANAGER=""
-    for f in ${!osInfo[@]}
-    do
-        if [[ -f $f ]];then
-            echo "Package manager: ${osInfo[$f]}"
-            PKG_MANAGER=${osInfo[$f]}
-        fi
-    done
-    #################################################
-    echo $PKG_MANAGER
-    # Install gpg and check that the install succeeded
-    sudo $PKG_MANAGER install gpg -y
-    if [ ! $? == 0 ]; then
-        echo "Failed to install GPG, please do so manually"
-        exit 1
-    fi
-    return 0
-}
-
-
 initialize_yadm() {
     # Make sure to initialize 
     git submodule init
@@ -53,9 +16,14 @@ initialize_yadm() {
     else
         # file does not exist at all 
         ln -s "$PWD/yadm/yadm" "$YADM_SYMLINK_DESTINATION"
+        if [ -L "$YADM_SYMLINK_DESTINATION" ]; then
+            # File is already symlinked no action needed
+            echo "Symlink success"
+        else
+            echo "Symlink failure"
+            exit 1
+        fi
     fi
-
-    
     git submodule update --remote yadm
 }
 
@@ -68,20 +36,27 @@ initialize_project_dotfiles() {
     echo "Parent Directory .git exists"
 
 
+    # Read repository name from parent directories remote url
     PARENT_REPOSITORY_REMOTE=`git -C .. config --get remote.origin.url`
     SUBMODULE_NAME="$(basename -s ".git" $PARENT_REPOSITORY_REMOTE)-dotfiles"
-    echo "Sub Module Parsed: $SUBMODULE_NAME"
+    echo "Submodule Parsed: $SUBMODULE_NAME"
 
+    if [ ! -d $SUBMODULE_NAME ] || [ ! -e "$SUBMODULE_NAME/.git" ]; then
+        echo "Submodule not found, adding $SUBMODULE_NAME"
+        source $PWD/scripts/add_project.sh <<< ""
 
-    if [ ! -e $SUBMODULE_NAME ]; then
-        echo "Parent repository name: $SUBMODULE_NAME does not match any stored dotfile projects"
-        exit 1
+        if [ $? -ne 0 ]; then
+            echo "Project add failure"
+            exit 1
+        else
+            echo "Project add success"
+            exit 0
+        fi
     fi
 
-
-    git submodule update --remote $SUBMODULE_NAME
+    # git submodule update --remote $SUBMODULE_NAME
 }
 
-install_gpg
+source $PWD/scripts/install_package.sh gpg
 initialize_yadm
 initialize_project_dotfiles
