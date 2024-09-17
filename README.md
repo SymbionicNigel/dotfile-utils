df916ed0005fcdda35389d771e5239fbe4fef28b
Nigel Krajcer <nphilmont2010@gmail.com>
2024-09-17 01:05:31 -0400

    feat: add script to create and include new submodules

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
 # DotFiles
 
+This project is meant to make use of [YADM](https://yadm.io/), [git submodules](https://git-scm.com/book/en/v2/Git-Tools-Submodules), and [gpg](https://gnupg.org/) to create separate repositories (git submodules) which will encypt the sensitive data needed to run other projects. The dotfiles repository is meant to be included as a submodule on any of my projects which need secrets file storage.
+
 ![Repository Structure](./media/structure.png)
 
+The same way that GNU/stow will map files into ones `$HOME` directory, YADM will store and apply those files for each project in the same directory structure they are meant to be used in.
+
 ## Basic Usage
+
+### Initializing on a new project
+
+Simply add this repository as a submodule to your current project and run the initialize script!
+If you are another user, change your remotes to point this to your own git repositories.
+
+```bash
+git submodule add <this-repositories-remote-url>
+cd dotfiles
+bash scripts/initialize.sh
+```
+
+Some expectations to take into consideration when running this:
+
+1. Your default branch in github has been set to `master`
+2. These scripts are meant to be ran from the `*/dotfiles` directory
+3. `sudo` priveledges will be asked for to install gpg and jq when running the initialize script if not installed already
 
 ### On Clone/Initialization
 
---
+++ scripts/add_project.sh
@@ -0,0 +1,62 @@
+#!/bin/bash Bash
+
+SUBMODULE_NAME=""
+PARENT_REPOSITORY_REMOTE=`git -C .. config --get remote.origin.url`
+
+get_submodule_name() {
+    # Check if parent git repository has been initialized 
+    if [ ! -e "./../.git" ]; then
+        echo "Parent Git Repository Not Initialized"
+        exit 1
+    fi
+    # Read repository name from parent directories remote url
+    SUBMODULE_NAME="$(basename -s ".git" $PARENT_REPOSITORY_REMOTE)-dotfiles"
+    
+    read -p $'\nPlease provide the name of the project\n(default will append "-dotfiles" to the parent repositories name )\n' -e -i "$SUBMODULE_NAME" SUBMODULE_NAME
+}
+
+make_new_folder() {
+    rm -rf "$PWD/$SUBMODULE_NAME"
+    echo "Submodule directory cleared"
+    mkdir "$SUBMODULE_NAME" 1> /dev/null
+    if  [ $? -ne 0 ] && [ ! -d $SUBMODULE_NAME ]; then
+        exit 1
+    elif [ $? == 0 ]; then
+        echo "$PWD/$SUBMODULE_NAME created"
+    else
+        echo "folder already created"
+    fi
+}
+
+initialize_submodule_repo() {
+    SUBMODULE_DIR="$PWD/$SUBMODULE_NAME"
+    # Prompt for git remote with default parsed from parent repository
+    read -p $'\nProvide a git remote to store this new module in:\n' -e -i "$(dirname $PARENT_REPOSITORY_REMOTE)/$SUBMODULE_NAME.git" REMOTE_URL
+    # Prompt for git default branch with default parsed from git config
+    read -p $'\nProvide a default branch name\n' -e -i "$(git config --get init.defaultBranch || echo "master")" BRANCH
+    if [ $BRANCH == "" ]; then
+        echo "Invalid Branch Name"
+        exit 1
+    fi
+
+    # TODO: add in step to go create the repo in github/gitlab
+    # TODO: Make the repository created here private by default, Look at this gist for inspiration for github https://gist.github.com/alexpchin/dc91e723d4db5018fef8
+    URL_ENCODED_REPO_NAME=$(jq -rn --arg x $SUBMODULE_NAME '$x|@uri')
+    read -p "Please follow this link to create a !!!PRIVATE!!! repository with the correct name of $SUBMODULE_NAME"$'\n'"https://github.com/new?name=$URL_ENCODED_REPO_NAME"$'\nPress enter when ready...'
+
+    echo "# $SUBMODULE_NAME" >> $SUBMODULE_NAME/README.md
+    git -C $SUBMODULE_DIR init
+    git -C $SUBMODULE_DIR add README.md
+    git -C $SUBMODULE_DIR commit -m "feat: initial commit"
+    git -C $SUBMODULE_DIR branch -M $BRANCH
+    git -C $SUBMODULE_DIR remote add origin $REMOTE_URL
+    git -C $SUBMODULE_DIR push -u origin $BRANCH
+    # Remove newly created repository to make room for submodule
+    rm -rf "$PWD/$SUBMODULE_NAME"
+    git submodule add $REMOTE_URL
+}
+
+source $PWD/scripts/install_package.sh jq
+get_submodule_name
+make_new_folder
+initialize_submodule_repo
