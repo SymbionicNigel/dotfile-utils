a84bf146ed6d722331dbdda0721c7919edfef94f
Nigel Krajcer <nphilmont2010@gmail.com>
2025-02-18 09:55:50 -0500

    fix: make scripts executable and add bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash Bash
+
+SCRIPT_DIR="$( cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd )"
+
+chmod 754 -Rc "$SCRIPT_DIR/scripts/"
--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -30,7 +30,6 @@ initialize_yadm() {
 }
 
 initialize_project_dotfiles() {
-    # Once YADM is initialized run submodule initialization for parent repository
     if [ ! -e "$SCRIPT_DIR/../.git" ]; then
         echo "Parent Git Repository Not Initialized"
         exit 1
@@ -42,7 +41,7 @@ initialize_project_dotfiles() {
     echo "Submodule Parsed: $SUBMODULE_NAME"
     if [ ! -d "$SUBMODULE_NAME" ] || [ ! -e "$SUBMODULE_NAME/.git" ]; then
         echo "Submodule not found, adding $SUBMODULE_NAME"
-        source "$SCRIPT_DIR/add_project.sh" <<< ""
+        "$SCRIPT_DIR/add_project.sh" <<< ""
         if [ $? -ne 0 ]; then
             echo "Project add failure"
             exit 1
@@ -51,12 +50,13 @@ initialize_project_dotfiles() {
             exit 0
         fi
     fi
+    # Once YADM is initialized run submodule initialization for parent repository
     # git submodule update --remote $SUBMODULE_NAME
 }
 
 SCRIPT_DIR="$( cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd )"
 
-sudo bash "$SCRIPT_DIR/install_package.sh" gpg
+sudo "$SCRIPT_DIR/install_package.sh" "gpg"
 initialize_yadm
 # initialize_project_dotfiles
 # source "$SCRIPT_DIR/bootstrap_dotfiles.sh" "$SUBMODULE_NAME"
